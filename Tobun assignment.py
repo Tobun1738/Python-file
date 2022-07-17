0050b31f66d34b3eaa103fbf1e06f6163f9c2991
Tobun1738 <108610398+Tobun1738@users.noreply.github.com>
2022-07-17 04:32:25 -0700

    Add files via upload

---
+++ Tobun assignment.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+question 2
+
+
+# In[45]:
+
+
+def add(n):
+    #n= input("enter a number")
+    result= int(n) + int(n+n) + int(n+n+n)
+    return(result)
+add('5')
+
+
+# In[52]:
+
+
+question 5
+
+
+# In[63]:
+
+
+def factoral(n):
+    n= input("enter a number")
+    result= 8*7*6*5*4*3*2*1
+    return(result)
+add(8)
+    
+
+
+# In[67]:
+
+
+Question1
+
+first_name= "Tobun"
+last_name= "Quadri"
+print(last_name,first_name)
+
+
+# In[68]:
+
+
+question3
+n=3
+n==9
+
+
+# In[70]:
+
+
+question7 
+
+x= (500>50)
+if x:
+    print('x is true')
+else:
+    print('x is not true')
+
+
+# In[71]:
+
+
+question4
+
+for i in range(2000,3200,7):
+    if not(5)
+    print(i)
+
+
+# In[ ]:
+
+
+
+
